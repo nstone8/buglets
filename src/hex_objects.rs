6a94080731f800c09502787ad5d7c13c56fd3b3e
Nick Stone <stone.nicholas@gmail.com>
2017-11-13 21:59:18 -0500

    Started adding machinery to dynamically draw pieces based on position in hexagonal coordinates. Currently will not compile

--- src/hex_objects.rs
+++ src/hex_objects.rs
@@ -4,35 +4,50 @@ use gtk::DrawingArea;
 
 pub trait Hex {
     fn draw_hex(p:Position, cr:&Context) {
-        //Draw a hexagon centered at p.x,p.y with max radius of 'scale'
+        //Draw a hexagon centered at p.i, p.j, p.k
+        let x=p.get_cartesian_x();
+        let y=p.get_cartesian_y();
         cr.set_source_rgb(0.0,0.0,0.0);
         cr.set_line_width(0.1);
         cr.new_path();
-        cr.move_to(1.0+p.x,0.0+p.y);
-        cr.line_to(0.5+p.x,0.866+p.y);
-        cr.line_to(-0.5+p.x,0.866+p.y);
-        cr.line_to(-1.0+p.x,0.0+p.y);
-        cr.line_to(-0.5+p.x,-0.866+p.y);
-        cr.line_to(0.5+p.x,-0.866+p.y);
+        cr.move_to(1.0+x,0.0+y);
+        cr.line_to(0.5+x,0.866+y);
+        cr.line_to(-0.5+x,0.866+y);
+        cr.line_to(-1.0+x,0.0+y);
+        cr.line_to(-0.5+x,-0.866+y);
+        cr.line_to(0.5+x,-0.866+y);
         cr.close_path();
         cr.stroke();
         
     }
 
-    fn get_draw_fn(&self) -> fn(&DrawingArea,&Context) -> Inhibit {
-        let draw_fn=|_:&DrawingArea,cr:&Context| -> Inhibit{
-            cr.scale(100.0,100.0);
-            Self::draw_hex(Position{x:2.0,y:2.0},cr);
-            Inhibit(false)
-        };
-        return draw_fn;
-    }
+    fn draw_fn(&self,cr:&Context);
 }
 pub struct Position{
-    x:f64,
-    y:f64
+    i:i32, //Number of hexes perpendicular to 'top' edge
+    j:i32, //Number of hexes perpendicular to the top right edge
+    k:i32, //Number of hexes perpendicular to the bottom right edge
+    h:i32, //Number of pieces stacked under this one
+} impl Position{
+    fn new(i:f64, j:f64, k:f64, h:f64) -> Position{
+        Position{i:i,j:j,k:k,h:h}
+    }
+    //Calculate equivalent cartesian coordinates. One unit corresponds to the max radius of each piece
+    pub fn get_cartesian_x(&self) -> f64{
+        0.7071*self.j+0.7071*self.k
+    }
+    pub fn get_cartesian_y(&self) -> f64{
+        self.i+0.7071*self.i+0.7071*self.k
+    }
 }
 
-pub struct EmptySpace{}
+pub struct EmptySpace{
+pos:Position
+}
 
-impl Hex for EmptySpace{}
+impl Hex for EmptySpace{
+    fn draw_fn(&self,cr:&Context){
+            Self::draw_hex(self.pos,cr);
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,31 @@ extern crate cairo;
 
 mod hex_objects;
 
+use std::vec::Vec;
+use std::boxed::Box;
 use gtk::prelude::*;
 use gtk::DrawingArea;
+use cairo::Context;
 use hex_objects::*;
 fn main() {
+    let scale=30;
+    
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
         return;
     }
-    let e = EmptySpace{};
+    let mut painter=Monet::new(scale);
+    let e = EmptySpace{pos:Position::new(0,0,0,0)};
+    painter.add_piece(e);
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
     window.set_title("buglets");
     let drawing_area = Box::new(DrawingArea::new)();
 
-    drawing_area.connect_draw(e.get_draw_fn());
+    drawing_area.connect_draw(|d:&DrawingArea,cr:&Context|{
+        cr.scale(painter.scale,painter.scale);
+        painter.draw_all_function(cr);
+        Inhibit(false)
+    });
     window.set_default_size(300, 300);
 
     window.connect_delete_event(|_, _| {
@@ -27,4 +38,21 @@ fn main() {
     window.show_all();
 
     gtk::main();
+}
+struct Monet{
+    scale: f64,
+    allPieces:Vec<Box<EmptySpace>>
+}impl Monet{
+    fn new(scale: f64) -> Monet{
+        Monet{scale:scale,allPieces:Vec::new()}
+    }
+    fn add_piece(&mut self,h: EmptySpace){
+        self.allPieces.push(Box::new(h));
+    }
+    fn draw_all_function(&self,cr:&Context){
+        for piece in &self.allPieces{
+            let draw_fn=piece.get_draw_fn();
+            draw_fn(cr);
+        }
+    }
 }
